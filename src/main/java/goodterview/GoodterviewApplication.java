0d4a2e81ee98906c3767bfa47e592e122bf4fb0f
psh94 <suran5281@gmail.com>
2025-06-15 11:57:14 +0900

    Initial commit

---
+++ src/main/java/goodterview/GoodterviewApplication.java
@@ -0,0 +1,13 @@
+package goodterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goodterview/GoodterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package goodterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
